c8f5c755fd5a2813824cbef996dc98e2de544167
Nicola Fabbrini <fabbrini.nicola@gmail.com>
2024-10-04 09:00:30 +1000

    BoundingBox to record

--- src/main/java/com/fabbroniko/gameobjects/BoundingBox.java
+++ src/main/java/com/fabbroniko/gameobjects/BoundingBox.java
@@ -3,23 +3,7 @@ package com.fabbroniko.gameobjects;
 import com.fabbroniko.environment.Dimension2D;
 import com.fabbroniko.environment.Position;
 
-public class BoundingBox {
-
-    private final Position position;
-    private final Dimension2D dimension;
-
-    public BoundingBox(final Position position, final Dimension2D dimension) {
-        this.position = position;
-        this.dimension = dimension;
-    }
-
-    public Position position() {
-        return position;
-    }
-
-    public Dimension2D dimension() {
-        return dimension;
-    }
+public record BoundingBox(Position position, Dimension2D dimension) {
 
     public boolean intersects(final BoundingBox boundingBox) {
         double x2 = this.position.getX() + this.dimension.width();
